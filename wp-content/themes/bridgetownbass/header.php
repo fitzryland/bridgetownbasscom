a21cb8482e8a8eb5a6796798148b9291541117fd
Fitz Ryland <fitzryland@gmail.com>
2013-02-27 18:11:35 -0800

    another header logo draft

--- wp-content/themes/bridgetownbass/header.php
+++ wp-content/themes/bridgetownbass/header.php
@@ -36,7 +36,7 @@
 		<div id="cover"></div>
 		<div class="fixed"></div>
 		<header>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo1_0.gif" width="498" height="570" alt="Btbheaderlogo" class="logo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo2_8.gif" width="498" height="447" alt="Btbheaderlogo2" class="logo">
 			<h1>
 				<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
 				<a href="<?php // echo home_url(); ?>">BRIDGETOWNBASS</a>
