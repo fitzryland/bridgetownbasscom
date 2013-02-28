4dee43b7716c7c0eff7752fe38226c54ec10632c
Fitz Ryland <fitzryland@gmail.com>
2013-02-27 20:14:27 -0800

    continued refinement of header gif

--- wp-content/themes/bridgetownbass/header.php
+++ wp-content/themes/bridgetownbass/header.php
@@ -36,7 +36,7 @@
 		<div id="cover"></div>
 		<div class="fixed"></div>
 		<header>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo2_11.gif" width="498" height="447" alt="Btbheaderlogo2" class="logo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo2_12.gif" width="498" height="447" alt="Btbheaderlogo2" class="logo">
 			<h1>
 				<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
 				<a href="<?php // echo home_url(); ?>">BRIDGETOWNBASS</a>
