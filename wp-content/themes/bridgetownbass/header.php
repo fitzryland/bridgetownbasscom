c703c9314dcb71453e6c6f4777c2b90c39df504e
Fitz Ryland <fitzryland@gmail.com>
2013-03-01 15:01:45 -0800

    added a mobile logo

--- wp-content/themes/bridgetownbass/header.php
+++ wp-content/themes/bridgetownbass/header.php
@@ -37,6 +37,7 @@
 		<div class="fixed"></div>
 		<header>
 			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_2.png" width="498" height="447" alt="Btbheaderlogo2" class="logo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_2mobile.png" width="316" height="284" alt="BRIDGETOWN BASS" class="logomobile">
 			<h1>
 				<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
 				<a href="<?php // echo home_url(); ?>">BRIDGETOWNBASS</a>
