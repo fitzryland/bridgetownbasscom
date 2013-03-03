b0fba732e529e52c394549a709d35b38fb792ecd
Fitz Ryland <fitzryland@gmail.com>
2013-03-03 14:10:55 -0800

    fixed home link in header

--- wp-content/themes/bridgetownbass/header.php
+++ wp-content/themes/bridgetownbass/header.php
@@ -39,7 +39,7 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_3mobile.png" width="316" height="284" alt="BRIDGETOWN BASS" class="logomobile">
 			<h1>
 				<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
-				<a href="<?php // echo home_url(); ?>">BRIDGETOWNBASS</a>
+				<a href="<?php echo home_url(); ?>">BRIDGETOWNBASS</a>
 			</h1>
 			<?php wp_nav_menu('primary'); ?>
 		</header>
