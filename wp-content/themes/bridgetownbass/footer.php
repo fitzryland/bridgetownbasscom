d3917f5095f3aed151560fc40ace4baf21334e2b
Fitz Ryland <fitzryland@gmail.com>
2013-03-06 21:42:39 -0800

    fliped header in footer, and removed footer from single page.

--- wp-content/themes/bridgetownbass/footer.php
+++ wp-content/themes/bridgetownbass/footer.php
@@ -1,28 +1,8 @@
 		</section>
 		<footer>
-			<h1><a href="#header">BRIDGETOWNBASS</a></h1>
-			<!--
-
-						<div class="social">
-							<div class="icon">
-								<a href="https://www.facebook.com/BRIDGETOWNBASS" target="_new" title="Bridgetown Bass Facebook">
-									<img class="f" src="<?php bloginfo('stylesheet_directory'); ?>/images/sprite3_3.png" width="336" height="96" alt="sprite_2">
-								</a>
-							</div>
-							
-							<div class="icon">
-								<a href="https://twitter.com/bridgetownbass" target="_new">
-									<img class="t" src="<?php bloginfo('stylesheet_directory'); ?>/images/sprite3_3.png" width="336" height="96" alt="sprite_2">
-								</a>
-							</div>
-							<div class="icon">
-								<a href="mailto:bridgetownbass@gmail.com" target="_new">
-									<img class="e" src="<?php bloginfo('stylesheet_directory'); ?>/images/sprite3_3.png" width="336" height="96" alt="sprite_2">
-								</a>
-							</div>
-						</div>
-					-->
-			
+			<?php if (!is_single()) {
+				include 'heading.php';
+			}; ?>
 		</footer>
 	<?php wp_footer(); ?>
 	</body>
--- wp-content/themes/bridgetownbass/header.php
+++ wp-content/themes/bridgetownbass/header.php
@@ -35,12 +35,8 @@
 	<body <?php body_class(); ?>>
 		<div class="fixed"></div>
 		<header>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_4.png" width="498" height="447" alt="Btbheaderlogo2" class="logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_3mobile.png" width="316" height="284" alt="BRIDGETOWN BASS" class="logomobile">
-			<h1>
-				<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
-				<a href="<?php echo home_url(); ?>">BRIDGETOWNBASS</a>
-			</h1>
-			<?php wp_nav_menu('primary'); ?>
+			
+			<?php include 'heading.php'; ?>
+			
 		</header>
 		<section>
