fb1c1504974752757fbf27af00beeff2f62c30db
Fitz Ryland <fitzryland@gmail.com>
2013-10-03 23:12:07 -0700

    begining of reset

--- wp-content/themes/bridgetownbass/heading.php
+++ wp-content/themes/bridgetownbass/heading.php
@@ -1,7 +1,7 @@
 <img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_4.png" width="498" height="447" alt="Btbheaderlogo2" class="logo">
 <img src="<?php echo get_template_directory_uri(); ?>/images/btbheaderlogo3_3mobile.png" width="316" height="284" alt="BRIDGETOWN BASS" class="logomobile">
-<h1>
-	<!--<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>-->
-	<a href="<?php echo home_url(); ?>">BRIDGETOWNBASS</a>
-</h1>
-<?php wp_nav_menu('primary'); ?>
+<!-- <h1>
+	<a href="<?php// echo home_url(); ?>"><span id="bridge">BRIDGE</span></br><span id="town">TOWN</span></br><span id="bass">BASS</span></a>
+	<a href="<?phpm // echo home_url(); ?>">BRIDGETOWNBASS</a>
+</h1> -->
+<?php // wp_nav_menu('primary'); ?>
