e8eca32a7d619cc009a5e06b0ff5237b63d02380
Fitz Ryland <fitzryland@gmail.com>
2013-11-30 11:22:05 -0800

    finished fuckin around

--- wp-content/themes/bridgetownbass/index.php
+++ wp-content/themes/bridgetownbass/index.php
@@ -7,28 +7,12 @@ date_default_timezone_set('UTC');
 $curTime = time() - 28800; // UTC time Converted to PST
 
 $querystr = "
-	SELECT $wpdb->posts.* ,
-	(UNIX_TIMESTAMP( wp_postmeta.meta_value ) + 97200) AS TIMESTAMP
-	FROM $wpdb->postmeta, $wpdb->posts
-	WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id
-	AND $wpdb->postmeta.meta_key =  'date'
-	AND (UNIX_TIMESTAMP( $wpdb->postmeta.meta_value ) + 97200) > $curTime
-	AND $wpdb->posts.post_status =  'publish'
-	AND $wpdb->posts.post_type =  'events'
-	ORDER BY $wpdb->postmeta.meta_value ASC
+SELECT *
+FROM wp_posts
 ";
-$pageposts = $wpdb->get_results($querystr, OBJECT);
-$lrgEventImgURLs = array();
+$pageposts = $wpdb->get_results($querystr);
 
-if ($pageposts):
-	global $post;
-foreach ($pageposts as $post):
-	setup_postdata($post);
-	
-	include 'event.php';
-	
-endforeach;
-else :
-endif;
-?>
-<?php include 'footer.php'; ?>
+print_r($pageposts);
+
+
+include 'footer.php'; ?>
